81956e311c2862f0abfc10e89dfc2b12c5b8afb5
Bart de Koning <bart.dekoning@deltares.nl>
2024-07-24 11:52:15 +0200

    Setup documentation structure

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,15 @@
 using Documenter
 using GaussTuranQuadrature
 
-makedocs(sitename="My Documentation")
+makedocs(;
+    sitename = "GaussTuranQuadrature.jl",
+    pages = [
+        "Home" => "index.md",
+        "Examples" => [
+            "Evaluating quadratures" => "evaluate_rule.md",
+            "Computing quadratures" => "compute_rule.md",
+        ],
+        "API" => "api.md",
+        "Theory" => "theory.md"
+    ]
+)
