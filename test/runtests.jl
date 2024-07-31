c150de7234d776c645683184afb102f0914618c8
lxvm <lorenzo@vanmunoz.com>
2024-07-31 15:46:33 -0400

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using SafeTestsets
 
-# @safetestset "aqua" begin include("aqua.jl") end
-# @safetestset "accuracy" begin include("accuracy.jl") end
-# @safetestset "quadrature" begin include("quadrature.jl") end
+@safetestset "aqua" begin include("aqua.jl") end
+@safetestset "accuracy" begin include("accuracy.jl") end
+@safetestset "quadrature" begin include("quadrature.jl") end
 @safetestset "TaylorDiff" begin include("taylordiff.jl") end
